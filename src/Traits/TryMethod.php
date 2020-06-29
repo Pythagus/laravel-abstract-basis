b4df05ba0c31d13ec11fda0eb1a1f94471790b25
Damien MOLINA <dmolina@etud.insa-toulouse.fr>
2020-06-29 20:39:42 +0200

    Ajax correction

--- src/Traits/TryMethod.php
+++ src/Traits/TryMethod.php
@@ -28,25 +28,45 @@ trait TryMethod {
         try {
             return $inner() ;
         } catch(Throwable $throwable) {
-            if(! $this->knownException($throwable)) {
-                LogException::add($throwable) ;
+            if(! $this->isKnownException($throwable)) {
+                $this->addThrowableInLog($throwable) ;
             }
 
             if(! is_null($out)) {
                 return $out($throwable) ;
             }
 
-            return $this->backError(
-                $this->knownException($throwable) ? $throwable->getMessage() : $this->weirdExceptionMessage($throwable)
-            ) ;
+            return $this->backError($this->getThrowableMessage($throwable)) ;
         }
+    }
+
+    /**
+     * Get the message of the throwable regarding its
+     * instance.
+     *
+     * @param Throwable $throwable
+     * @return string
+     */
+    private function getThrowableMessage(Throwable $throwable) {
+        return $this->isKnownException($throwable)
+            ? $throwable->getMessage()
+            : $this->weirdExceptionMessage($throwable) ;
+    }
+
+    /**
+     * @param Throwable $throwable
+     */
+    protected function addThrowableInLog(Throwable $throwable) {
+        try {
+            LogException::add($throwable) ;
+        } catch(Throwable $throwable) {}
     }
 
     /**
      * @param Throwable $throwable
      * @return bool
      */
-    protected function knownException(Throwable $throwable) {
+    protected function isKnownException(Throwable $throwable) {
         return $throwable instanceof MyException ;
     }
 
@@ -62,13 +82,13 @@ trait TryMethod {
      * @param callable $inner
      * @return JsonResponse
      */
-    protected function tryAjax(callable $inner) {
+    protected function ajax(callable $inner) {
         return $this->try(function() use ($inner) {
-            $html = $inner() ;
+            $data = $inner() ;
 
-            return response()->json(compact('html')) ;
-        }, function(Throwable $e) {
-            $text = $e->getMessage() ;
+            return response()->json($data) ;
+        }, function(Throwable $throwable) {
+            $text = $this->getThrowableMessage($throwable) ;
 
             return response()->json(compact('text'), 300) ;
         }) ;
