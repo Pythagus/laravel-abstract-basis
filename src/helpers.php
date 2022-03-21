a319c0398455f46f5caf4d7e4b902d884b1ddd0e
Pythagus <dmolina@etud.insa-toulouse.fr>
2022-03-21 08:49:08 +0100

    Get the asset last modified time

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Pythagus\LaravelAbstractBasis\Maintenance;
 
 if(! function_exists('maintenance_date')) {
@@ -10,5 +11,25 @@ if(! function_exists('maintenance_date')) {
      */
     function maintenance_date() {
         return Maintenance::next() ;
+    }
+}
+
+if(! function_exists('last_modified_time')) {
+    /**
+     * Get the last modified time from
+	 * the given asset file.
+     *
+     * @param string $url
+     * @return string
+     */
+    function last_modified_time($url) {
+		$version = 0 ;
+
+        try {
+			$absolute = $_SERVER['DOCUMENT_ROOT'] . parse_url($url, PHP_URL_PATH) ;
+			$version  = File::lastModified($absolute) ;
+		} catch(Throwable $ignored) {}
+
+        return $version ;
     }
 }
