0d3791c46d0f4c159c69c9833856b301264004a6
Pythagus <dmolina@etud.insa-toulouse.fr>
2022-09-25 12:37:56 +0200

    :sparkles: Manage custom exceptions

---
+++ src/Contracts/KnownException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pythagus\LaravelAbstractBasis\Contracts;
+
+/**
+ * Interface KnownException
+ * @package Pythagus\LaravelAbstractBasis\Contracts
+ *
+ * @author: Damien MOLINA
+ */
+interface KnownException {}
